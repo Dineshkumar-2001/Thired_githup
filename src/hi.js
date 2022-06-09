2898ef293ae51908a1d77802793606defa624fad
Dineshkumar-2001 <dineshkumar@valorpaytech.com>
2022-06-09 17:27:43 +0530

    ok

---
+++ src/hi.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class hi extends Component {
+  render() {
+    return (
+      <div>
+          <h4>hii_1</h4>
+          <h4>hii_2</h4>
+          <h4>hii_3</h4>
+          <h4>hii_4</h4>
+      </div>
+    )
+  }
+}
